8d8a7eace9060d7e313d8cfb0483e65dd8424e2c
Alexander <92460691+san27sin@users.noreply.github.com>
2022-01-27 22:45:47 +0300

    Add files via upload

--- Lesson_3/Lesson_3.cpp
+++ Lesson_3/Lesson_3.cpp
@@ -1,1 +1,46 @@
+﻿#include <iostream>
 
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	
+	char reply;
+	do
+	{
+		std::cout << "Введите число для проверки: " << std::endl;
+		int choice{ 0 };
+		std::cin >> choice;
+
+		if (choice == 0 || choice == 1 || choice == 2 || choice == 3 || choice == 5)
+		{
+			if (choice == 0 || choice == 1)
+			{
+				std::cout << "Исключение" << std::endl;
+			}
+			else
+			{
+				std::cout << "Простое число" << std::endl;
+			}
+			
+		}
+
+		else
+		{
+			if (choice % 2 == 0 || choice % 3 == 0 || choice % 5 == 0)
+			{
+				std::cout << "Не простое число" << std::endl;
+			}
+
+			else
+			{
+				std::cout << "Простое число" << std::endl;
+			}
+		}
+
+		std::cout << "Хотите продолжить? Y/N " << std::endl;
+		std::cin >> reply;
+
+	} while (reply == 'Y');
+	
+
+}
