0c5de86439e819d672f50d7950528844ed5c6b91
Alexander <92460691+san27sin@users.noreply.github.com>
2022-01-27 21:30:22 +0300

    Add files via upload

--- Lesson_3/Lesson_3.cpp
+++ Lesson_3/Lesson_3.cpp
@@ -1,1 +1,58 @@
+﻿#include <iostream>
 
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	char reply;
+	do
+	{
+		std::cout << "Введите число для проверки: " << std::endl;
+		int i;
+		std::cin >> i;
+
+		if (i == 1)
+		{
+			std::cout << "Именно чтобы обеспечить единственность в этой теореме, единица не считается простым числом!" << std::endl;
+		}
+
+		if (i % 2 == 0)
+		{
+
+			if (i == 2)
+			{
+				std::cout << "Простое число!" << std::endl;
+			}
+			else
+			{
+				std::cout << "Не простое!" << std::endl;
+			}
+
+		}
+
+
+		else if (i == 3)
+		{
+			std::cout << "Простое число!" << std::endl;
+		}
+
+
+		else
+		{
+
+			if (i % 3 == 0)
+			{
+				std::cout << "Не простое число!" << std::endl;
+			}
+
+			else
+			{
+				std::cout << "Простое число!" << std::endl;
+			}
+		}
+		std::cout << "Хотите продолжить? Y/N " << std::endl;
+		std::cin >> reply;
+
+	} while (reply == 'Y');
+
+}
